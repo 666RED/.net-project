8134d2756b22f2cd3b9c896b75fc839b19baa81e
ChiiSung <lingchiisung@gmail.com>
2024-01-11 16:22:48 +0800

    cs

--- register.aspx.cs
+++ register.aspx.cs
@@ -13,16 +13,36 @@ namespace Net_project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+             ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
         }
 
         protected void UserRegister(object sender, EventArgs e)
         {
-
+            
 
             SqlConnection con = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog = TestDatabase; Trusted_Connection = true; Pooling = False");
             try
             {
+                String command = "SELECT * From librarian WHERE email = @email OR telephone = @telephone";
+                SqlDataAdapter cmdCheck = new SqlDataAdapter;
+                cmdCheck.InsertCommand = new SqlCommand(command, con);
+
+                cmdCheck.InsertCommand.Parameters.Add("@email", SqlDbType.VarChar).Value = email.Value;
+                cmdCheck.InsertCommand.Parameters.Add("@telephone", SqlDbType.VarChar).Value = telephone.Value;
+                SqlDataReader reader = cmdCheck.InsertCommand.ExecuteReader();
+                if (reader.Read())
+                {
+                    if (reader[0].Equals(email.Value))
+                    {
+                        validEmail.Text = "Email already exist";
+                    }
+                    else
+                    {
+                        validEmail.Text = "";
+                    }
+                }
+    
+
                 con.Open();
                 SqlDataAdapter cmd = new SqlDataAdapter();
                 cmd.InsertCommand = new SqlCommand("INSERT INTO librarian VALUES(@librarainId, @password, @email, @username, @gender, @telephone) ", con);
